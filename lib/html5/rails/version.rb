bf578ff4660d3a36270926a701bfead8911abcf1
Peter Gumeson <gumeson@gmail.com>
2011-09-21 00:02:18 -0700

    Bumped gemspec to v0.0.2

--- lib/html5/rails/version.rb
+++ lib/html5/rails/version.rb
@@ -1,7 +1,7 @@
 module Html5
   module Rails
-    VERSION = "0.0.1"
-    COMPASS_HTML5_VERSION = "0.0.1"
+    VERSION = "0.0.2"
+    COMPASS_HTML5_VERSION = "0.0.2"
     JQUERY_VERSION = "1.6.1"
     MODERNIZER_VERSION = "2.0"
     RESPOND_VERSION  = "625d77dc1aed97baf0ae"
