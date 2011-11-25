207d60e75c1b50e2a7d847a0e165de96b2bf7091
Hector Satre <moktin@gmail.com>
2011-11-25 16:38:52 +0100

    ruby 1.8* compatibility

--- lib/generators/html5/layout/layout_generator.rb
+++ lib/generators/html5/layout/layout_generator.rb
@@ -3,20 +3,20 @@ module Html5
     class LayoutGenerator < ::Rails::Generators::Base
       source_root File.expand_path('../../../../../app/views', __FILE__)
 
-      argument :name, type: :string,
-                      required: false,
-                      default: "application",
-                      desc: "The layout name"
-                      
-      class_option :partials, type: :boolean, 
-                              default: false,
-                              desc: "Generate partials"
+      argument :name, :type => :string,
+                      :required => false,
+                      :default => "application",
+                      :desc => "The layout name"
+
+      class_option :partials, :type => :boolean,
+                              :default => false,
+                              :desc => "Generate partials"
 
       def generate_layout
         remove_file "app/views/layouts/#{ name.underscore }.html.erb"
         copy_file "layouts/application.html.haml", "app/views/layouts/#{ name.underscore }.html.haml"
       end
-      
+
       def copy_partials
         puts "OPTIONS #{options.inspect}"
         if options.partials?
