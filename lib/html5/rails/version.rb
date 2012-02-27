21ca6eba0d141bd5ea0c1005730ec5d8756e4012
Peter Gumeson <gumeson@gmail.com>
2012-02-26 20:26:36 -0800

    New version 0.0.5

--- lib/html5/rails/version.rb
+++ lib/html5/rails/version.rb
@@ -1,7 +1,6 @@
 module Html5
   module Rails
-    VERSION = "0.0.4"
-    COMPASS_H5BP_VERSION = "0.0.2"
-    MODERNIZER_VERSION = "2.0.6"
+    VERSION = "0.0.5"
+    COMPASS_H5BP_VERSION = "0.0.5"
   end
 end
